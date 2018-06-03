d8a34080f27b91fab7b73482a6927336f53d627e
warrenpu <1210513673@qq.com>
2018-06-03 19:37:39 -0400

    apply default connector from catalina

--- src/org/py4j/startup/Bootstrap.java
+++ src/org/py4j/startup/Bootstrap.java
@@ -1,11 +1,23 @@
 package org.py4j.startup;
 
 
-import org.py4j.connector.http.HttpConnector;
+import org.apache.catalina.connector.http.HttpConnector;
+import org.py4j.SimpleContainer;
 
 public final class Bootstrap {
     public static void main(String[] args) {
         HttpConnector connector = new HttpConnector();
-        connector.start();
+        SimpleContainer container = new SimpleContainer();
+        connector.setContainer(container);
+        try {
+            connector.initialize();
+            connector.start();
+
+            // make the application wait until we press any key.
+            System.in.read();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
