63d98c4d2e3e12d3cc939d69e335dcc16f6d8f5b
warrenpu <1210513673@qq.com>
2018-06-02 23:18:44 -0400

    initial project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # py4j-tomcat
+
+this is just for self study of tomcat framework.
---
+++ src/org/py4j/Bootstrap.java
@@ -0,0 +1,9 @@
+package org.py4j;
+
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        System.out.println("initial success");
+    }
+
+}
